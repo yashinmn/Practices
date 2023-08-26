974e9aed8dbefa476206abdaec0d01fa5014a58e
yashinmn <132077805+yashinmn@users.noreply.github.com>
2023-08-26 19:59:34 +0530

    Add files via upload
    Multiplication table Implementation in Java

---
+++ MultipleofIntegers.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+public class MultipleofIntegers {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        for(int i=1;i<=10;i++)
+        {
+            int k=i*N;
+            System.out.printf("%d x %d = %d%n",N,i,k);
+        }
+        bufferedReader.close();
+    }
+}
