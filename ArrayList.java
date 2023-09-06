9262dc6451fef02e102efad1ab85b1e78c22236e
yashinmn <yashinhaikoo@gmail.com>
2023-09-06 18:50:41 +0530

    Arraylist in Java

---
+++ ArrayList.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+
+class ArrayList {
+  public static void main(String[] args)
+{
+    ArrayList<String> fruits = new ArrayList<>();
+   
+    fruits.add("Apple");
+    fruits.add("Mango");
+    fruits.add("Orange");
+    System.out.println("ArrayList: " + fruits);
+ }
+}
