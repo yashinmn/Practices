71b282183d9ceb3a1bdef5e355d7ee41145a462a
yashinmn <yashinhaikoo@gmail.com>
2023-09-23 23:45:04 +0530

    linkedhashmap in Java

---
+++ linkedHashMap.java
@@ -0,0 +1,15 @@
+import java.util.LinkedHashMap;
+
+class linkedHashMap {
+    public static void main(String[] args) {
+       
+        LinkedHashMap<String, Integer> evenNumbers = new LinkedHashMap<>();
+        evenNumbers.put("Two", 2);
+        evenNumbers.put("Four", 4);
+        System.out.println("LinkedHashMap1: " + evenNumbers);
+
+        LinkedHashMap<String, Integer> numbers = new LinkedHashMap<>(evenNumbers);
+        numbers.put("Three", 3);
+        System.out.println("LinkedHashMap2: " + numbers);
+    }
+}
